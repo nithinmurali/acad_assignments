3ff7ffdca0150e58aa47f151002457d1bf9b1cbc
nithinmurali <imnmfotmal@gmail.com>
2016-04-08 06:37:02 +0530

    added freq flot

--- toycipher_impl/enc.py
+++ toycipher_impl/enc.py
@@ -1,3 +1,6 @@
+#! bin/python
+from collections import Counter
+import matplotlib.pyplot as plt
 
 def tonum(input):
         output = []
@@ -31,14 +34,14 @@ def encrypt(text, key):
     cipher = ""
 
     numblocks = len(text)/blocklength;
-    print "num of blocks : ", numblocks ," ; ", len(text)
+    #print "num of blocks : ", numblocks ," ; ", len(text)
     if numblocks*blocklength < len(text):
         padlength = blocklength - len(text)%blocklength
         for i in xrange(0,padlength):
             text += str('a')
         numblocks+=1
     
-    print "text appended : ", text ,"  " , len(text), numblocks
+    #print "text appended : ", text ,"  " , len(text), numblocks
 
     #initial block
     p0 = text[0*blocklength : (0+1)*blocklength]
@@ -50,10 +53,6 @@ def encrypt(text, key):
     ci = [(x+y+z)%25+1 for x,y,z in zip(ci, k0, p0)]
     cipher = ''.join(chr(i) for i in tonumx(ci))
     cp = ci
-
-    # print "c0 : ", ci
-    # print "p0 : ", p0
-    # print "c0 : ", cp
 
     #other blocks
     for i in xrange(1,numblocks):
@@ -76,13 +75,13 @@ def encrypt(text, key):
         pass
 
     print cipher
+    return cipher
 
 
 def decrypt(cipher, key):
     blocklength = 10;
     iv = "qwertyuiop"
     text =""
-    print "############# decryption ######################"
     numblocks = len(cipher)/blocklength;
     print "num of blocks : ", numblocks ," ; ", len(cipher)
     
@@ -100,41 +99,55 @@ def decrypt(cipher, key):
     text = ''.join(chr(i) for i in tonumx(pi))
     cp = c0
 
-    # print "c0 : ", c0
-    # print "k0 : ", k0
-    # print "iv : ", iv
-    # print "p0 : ", pi
-    # print "p0 : ", text
-
     #other blocks
     for i in xrange(1,numblocks):
         #print "################### round ", i
         ci = cipher[i*blocklength : (i+1)*blocklength]
-        #print "c0 txt : ", ci
-        ci = tonum(ci)
-        
+        ci = tonum(ci)    
         k0 = [(x+1)%25+1 for x in k0]
         rotate(k0, sum(cp))
-        #print "c0 : ", ci
-        #print "rotated key : ", k0
-
+    
         pi = [ (x-y-1)%25 for x,y in zip(ci, k0)]
         txtf = ''.join(chr(i) for i in tonumx(pi))
         text += txtf
-        #print "pi : ", pi
         #print "text : " , txtf
         cp = ci
         pass
 
     print text
-    pass
+    return text
+
+def plot_freq(text, n):
+    if n==2:
+        c1 = Counter( [text[i:i+n] for i in range(0, len(text), n)] )
+        text = text[1:]
+        c2 = Counter( [text[i:i+n] for i in range(0, len(text), n)] )
+
+        c1.update(c2)
+    elif n==1:
+        c1 = Counter(text)
+        tot = len(text)
+    else:
+        return
+    #print c1
+    print c1.most_common(10)
+    tot = len(c1)
+    c1 = { key: value/float(tot) for key, value in c1.iteritems() }
+    plt.bar(range(len(c1)), c1.values(), align='center')
+    plt.xticks(range(len(c1)), c1.keys())
+    plt.show()
 
 if __name__ == '__main__':
-    text = "a home is a dwellingplace used as a permanent or semipermanent residence for an individual, family, household or several families in a tribe. it is often a house, apartment, or other building, or alternatively a mobile home, houseboat, yurt or any other portable shelter"
+    filename = "./text"
+    text = open(filename, 'r').read()
     text=text.replace(".", "")
     text=text.replace(",", "")
     text = ''.join(text.split())
-    #encrypt(text,"wertyuopqi")
+    cipher = encrypt(text,"wertyuopqi")
+    open('./cipher', 'w').write(cipher)
 
-    cipher ="plmyydcakwetffpegdupgepdhamvltvyzolpplpilaksnvkznxvkukcpdfpxpytynvlqhfhdldpvkfshzvrzlelpwmuncfxknaloybzyfzpdvsvwgayxvgxzlllymvwdlbdhiwsqcwfepakrldkvpwopgnmpmwhikencamsckqhbqfsqdhpfozsadmcqdpkoxbbadrdmgdnbezwzhbaccuwyanac"
-    decrypt(cipher,"wertyuopqi")
+    #decrypt(cipher,"wertyuopqi")
+    
+    #for plotting frequencies
+    plot_freq(cipher,1)
+    plot_freq(cipher,2)
